e595535681b7cc7011f6e8630d739586e3a4680f
karol.stosiek@gmail.com <karol.stosiek@gmail.com@867b7001-1042-0410-a24e-53abcf533597>
2008-01-14 18:04:35 +0000

    Batch file changed.

--- trunk/opl/src/make.bat
+++ trunk/opl/src/make.bat
@@ -1,6 +1,14 @@
+@echo off
+echo Compiling to destination ../bin/opl.exe
 ocamlyacc parser.mly
 ocamlc -c parser.mli
 ocamlc -c parser.ml
 ocamllex lexer.mll
 ocamlc -c lexer.ml
-ocamlc -o opl.exe lexer.cmo parser.cmo opl.ml
+ocamlc -o ../bin/opl.exe lexer.cmo parser.cmo opl.ml
+del *cmi 
+del *cmo
+del lexer.ml
+del parser.mli
+del parser.ml
+echo Compiled successfully.
